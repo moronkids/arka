f64b992941776f231156504942896b99a4986088
Ardani <sulistyoardani@gmail.com>
2019-07-02 14:03:30 +0700

    no 3 100% bener mas

--- soalno3.py
+++ soalno3.py
@@ -1,25 +1,31 @@
 string = ""
 bar = 1
-
+z = 1
+i = 1
 x = int(input("Masukkan angka :"))
-print ("\n")
+
 # Looping Baris
 while bar <= x:
 	kol = bar
-	# Looping Kolom Spasi Kosong
-	while kol > 1:
-		string = string + "   "
+	# Looping Kolom
+	batas = kol - 1
+	while kol > 0:
+		if kol > 1:
+			string = string + "   "
+		else:
+			string = string + " * "
 		kol = kol - 1
-	# Looping Kolom Bintang Sisi Kiri
-	kiri = 0
-	while kiri <= (x - bar):
-		string = string + " * "
-		kiri = kiri + 1
-	# Looping Kolom Bintang Sisi Kanan
-	kanan = kiri
-	while kanan > 1:
-		string = string + " * "
-		kanan = kanan - 1
 
-	string = string + "\n\n"
+	if bar == 1:
+		string = string + " * " * ((x-z)*2)
+	elif bar == x:
+		string = string
+	else :
+		string = string + "   " * ((x-z)*2-1) + " * "
+
+
+	z = z + 1
+	string = string + "\n"
 	bar = bar + 1
+	i = i + 1
+print (string)
